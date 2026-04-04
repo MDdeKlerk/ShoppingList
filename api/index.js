2af18b24e129115035e11dba1da3495a1f33e6ec
MDdeKlerk <marethadeklerk@gmail.com>
2026-04-04 09:59:49 +0200

    Backend Get functions
    Backend logic for displaying the shopping list and item details (Get/items and Get/items/:id)

--- api/index.js
+++ api/index.js
@@ -7,3 +7,25 @@ const PORT = 5000;
 app.use(cors());
 app.use(express.json());
 
+
+//initialise variables
+let items = [];
+let autoID = 1;
+
+//GET /items (display shopping list)
+app.get('/items', (req, res) => {
+    const shoppingList = items.map(({id, title, done}) => ({
+        id,
+        title,
+        done
+    }));
+    res.json(shoppingList);
+});
+
+//GET /items/:id (view an item including details)
+app.get('/items/:id', (req,res) => {
+    const itemDetail = items.find(i => i.id === Number(req.params.id));
+    res,json(itemDetail);
+});
+
+//POST /items (Create a new item on the list)
